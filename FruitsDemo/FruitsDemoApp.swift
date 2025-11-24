7a9894a9c7e2cd846c72778caa235c3299fcea97
GerardJalonG <gerard.jalon@gmail.com>
2025-11-24 17:22:56 +0100

    add list and complete mvvm to connect all the files

--- FruitsDemo/FruitsDemoApp.swift
+++ FruitsDemo/FruitsDemoApp.swift
@@ -2,9 +2,10 @@ import SwiftUI
 
 @main
 struct FruitsDemoApp: App {
+    @StateObject var fruitStore = FruitStore()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(fruitStore)
         }
     }
 }
--- FruitsDemo/Views/ContentView.swift
+++ FruitsDemo/Views/ContentView.swift
@@ -4,14 +4,16 @@ import SwiftUI
 //TODO: Create a list
 //TODO: Create the navigation
 struct ContentView: View {
+    @EnvironmentObject var fruitStore:FruitStore
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(fruitStore.fruits){ fruit in
+            FruitRowView(fruit: fruit)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(FruitStore())
     }
 }
