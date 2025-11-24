3595b3a770108dbb085cef1ecbcb28244e7870e5
GerardJalonG <gerard.jalon@gmail.com>
2025-11-24 17:37:46 +0100

    add remove on list

--- FruitsDemo/Views/ContentView.swift
+++ FruitsDemo/Views/ContentView.swift
@@ -7,12 +7,17 @@ struct ContentView: View {
     @EnvironmentObject var fruitStore:FruitStore
     var body: some View {
         NavigationView{
-            List(fruitStore.fruits){ fruit in
-                NavigationLink(destination: DetailFruitView(fruit: fruit)) {
-                    FruitRowView(fruit: fruit)
-                }
+            List{
+                ForEach(fruitStore.fruits) { fruit in
+                    NavigationLink(destination: DetailFruitView(fruit: fruit)) {
+                        FruitRowView(fruit: fruit)
+                    }
+                }.onDelete(perform: remove)
             }.navigationBarTitle(Text("Fruits"))
         }
+    }
+    func remove( at offsets: IndexSet){
+        fruitStore.fruits.remove(atOffsets: offsets)
     }
 }
 
